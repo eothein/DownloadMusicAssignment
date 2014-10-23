1ef665a923e64edb094fc9a04d899f3f9892b315
Jens Buysse <jens.buysse@hogent.be>
2014-10-23 18:27:09 +0200

    Commit

---
+++ src/main/java/com/example/jbuy519/asynctaskexample/DownloadMusic.java
@@ -0,0 +1,81 @@
+package com.example.jbuy519.asynctaskexample;
+
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+
+/**
+ * Created by jbuy519 on 08/10/2014.
+ */
+public class DownloadMusic extends AsyncTask<String, String, String> {
+
+    private static String TAG = "DownloadMusic";
+
+    private MusicInterface mI;
+
+    public DownloadMusic(MusicInterface mI){
+        this.mI = mI;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+        mI.downloadFinished();
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+
+    @Override
+    protected void onProgressUpdate(String... values) {
+        super.onProgressUpdate(values);
+        int value = Integer.parseInt(values[0]);
+        mI.setProgressStatus(value);
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+        int count;
+        URL url = null ;
+        HttpURLConnection connection = null;
+        OutputStream output = null;
+        InputStream input = null;
+        try{
+            url = new URL(params[0]);
+            connection = (HttpURLConnection)url.openConnection();
+            int lenghtOfFile = connection.getContentLength();
+            input = new BufferedInputStream(url.openStream(),10*1024);
+            String path = Environment.getExternalStorageDirectory().getPath()+"/test.mp3";
+            output = new FileOutputStream(Environment.getExternalStorageDirectory().getPath()+"/test.mp3");
+            byte[] data = new byte[1024];
+            long total = 0;
+            while((count = input.read(data))!=-1) {
+                total+= count;
+                String progress = "" + (int) ((total * 100) / lenghtOfFile);
+                Log.i(TAG,total+ "     " +progress);
+                publishProgress(progress);
+                output.write(data,0,count);
+            }
+            output.flush();
+            output.close();
+            input.close();
+            connection.disconnect();
+
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage());
+        }
+        return null;
+    }
+}
